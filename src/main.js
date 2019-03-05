9e7146b3d1aec52cf184d45658659e76066cd8dd
gy <982008488@qq.com>
2019-03-05 21:49:33 +0800

    add main.js

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+//Vue中的轮播插件
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+import 'swiper/dist/css/swiper.css'
 // 解决移动端三百毫秒点击延迟插件
 import fastClick from 'fastclick'
 import reset from 'styles/reset.css'
@@ -10,6 +13,7 @@ import iconfont from 'styles/iconfont.css'
 
 Vue.config.productionTip = false
 fastClick.attach(document.body);
+Vue.use(VueAwesomeSwiper)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
